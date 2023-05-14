446db60de64f9a7ea258d032b6e28425b9320deb
John Peel <john@dgby.org>
2023-05-14 12:04:45 -0400

    fixed issue with Hero class gear

--- IlvlTooltip.lua
+++ IlvlTooltip.lua
@@ -7,7 +7,7 @@ local RANGES = {
 }
 
 local UPGRADE_CLASS_START = string.find(ITEM_UPGRADE_TOOLTIP_FORMAT_STRING, "%%s")
-local UPGRADE_LEVEL = strsub(ITEM_UPGRADE_TOOLTIP_FORMAT_STRING, 0, UPGRADE_CLASS_START - 1)
+local UPGRADE_LEVEL = strsub(ITEM_UPGRADE_TOOLTIP_FORMAT_STRING, 1, UPGRADE_CLASS_START - 1)
 
 local function OnTooltipSetItem(tooltip, tooltipData)
     local itemName, itemLink = TooltipUtil.GetDisplayedItem(tooltip)
@@ -22,23 +22,23 @@ local function OnTooltipSetItem(tooltip, tooltipData)
 
     local upgradeLine = 3
     local upgradeText = tooltipData.lines[upgradeLine].leftText
-    while(strsub(upgradeText, 0, UPGRADE_CLASS_START - 1) ~= UPGRADE_LEVEL) do
-        upgradeLine = upgradeLine + 1
-        upgradeText = tooltipData.lines[upgradeLine].leftText
+    local upgradeTextLength = strlen(upgradeText)
 
+    while(upgradeTextLength < UPGRADE_CLASS_START + 4 or strsub(upgradeText, 1, UPGRADE_CLASS_START - 1) ~= UPGRADE_LEVEL) do
         if upgradeLine > 5 then
             return
         end
+
+        upgradeLine = upgradeLine + 1
+        upgradeText = tooltipData.lines[upgradeLine].leftText
+        upgradeTextLength = strlen(upgradeText)
     end
 
-    local upgradeTextLength = strlen(upgradeText)
-    if upgradeTextLength >= 24 then
-        local upgradeClass = strlower(strsub(upgradeText, UPGRADE_CLASS_START, upgradeTextLength - 4));
-        local range = RANGES[upgradeClass];
-        if range then
-            tooltipData.lines[upgradeLine - 1].rightColor = DISABLED_FONT_COLOR
-            tooltipData.lines[upgradeLine - 1].rightText = range
-        end
+    local upgradeClass = strlower(strsub(upgradeText, UPGRADE_CLASS_START, upgradeTextLength - 4));
+    local range = RANGES[upgradeClass];
+    if range then
+        tooltipData.lines[upgradeLine - 1].rightColor = DISABLED_FONT_COLOR
+        tooltipData.lines[upgradeLine - 1].rightText = range
     end
 end
 
