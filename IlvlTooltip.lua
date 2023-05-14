a7e51b572345b440dfcaced7352d206f6b691b06
John Peel <john@dgby.org>
2023-05-14 10:55:30 -0400

    fixed raid finder gear & localization issues

--- IlvlTooltip.lua
+++ IlvlTooltip.lua
@@ -1,10 +1,13 @@
-local ranges = {
+local RANGES = {
     explorer = "(376-398)",
     adventurer = "(389-411)",
     veteran = "(402-424)",
     champion = "(415-437)",
     hero = "(428-441)",
 }
+
+local UPGRADE_CLASS_START = string.find(ITEM_UPGRADE_TOOLTIP_FORMAT_STRING, "%%s")
+local UPGRADE_LEVEL = strsub(ITEM_UPGRADE_TOOLTIP_FORMAT_STRING, 0, UPGRADE_CLASS_START - 1)
 
 local function OnTooltipSetItem(tooltip, tooltipData)
     local itemName, itemLink = TooltipUtil.GetDisplayedItem(tooltip)
@@ -13,18 +16,28 @@ local function OnTooltipSetItem(tooltip, tooltipData)
     end
 
     local itemType = select(6, GetItemInfo(itemLink))
-    if itemType ~= "Armor" and itemType ~= "Weapon" then
+    if itemType ~= ARMOR and itemType ~= WEAPON then
         return
     end
 
-    local upgradeText = tooltipData.lines[3].leftText
+    local upgradeLine = 3
+    local upgradeText = tooltipData.lines[upgradeLine].leftText
+    while(strsub(upgradeText, 0, UPGRADE_CLASS_START - 1) ~= UPGRADE_LEVEL) do
+        upgradeLine = upgradeLine + 1
+        upgradeText = tooltipData.lines[upgradeLine].leftText
+
+        if upgradeLine > 5 then
+            return
+        end
+    end
+
     local upgradeTextLength = strlen(upgradeText)
     if upgradeTextLength >= 24 then
-        local upgradeClass = strlower(strsub(upgradeText, 16, upgradeTextLength - 4));
-        local range = ranges[upgradeClass];
+        local upgradeClass = strlower(strsub(upgradeText, UPGRADE_CLASS_START, upgradeTextLength - 4));
+        local range = RANGES[upgradeClass];
         if range then
-            tooltipData.lines[2].rightColor = DISABLED_FONT_COLOR
-            tooltipData.lines[2].rightText = range
+            tooltipData.lines[upgradeLine - 1].rightColor = DISABLED_FONT_COLOR
+            tooltipData.lines[upgradeLine - 1].rightText = range
         end
     end
 end
